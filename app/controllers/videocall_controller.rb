7ac5871de30a29d21d116e20725310dc6a233357
Jose Carlos <sipox11@gmail.com>
2017-09-29 23:16:01 +0200

    Add token dynamically from controller

--- app/controllers/videocall_controller.rb
+++ app/controllers/videocall_controller.rb
@@ -1,4 +1,10 @@
 class VideocallController < ApplicationController
   def vidyoconnector
+  	@token = generate_token
   end
+
+  private
+  	def generate_token
+  		return "cHJvdmlzaW9uAGNpY2xvdXNlckAxYmU4NjUudmlkeW8uaW8ANjM2NzM5NDgzOTQAADYzMDdkNmU2NjNiOGZkZTIzNzA4ODA0NWI2YmFmZDM3ODNhYjIyYmY5ZmE4YjBiN2FkZTdlNTVkMjIwM2Y4NWIyNzk2MTcwYzc0ZjllOTIyZDgwYmNjYWM0M2YzZWY2MA=="
+  	end
 end
