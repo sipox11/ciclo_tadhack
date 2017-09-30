aebb3d72fb4ccf712600e014885f60610293707e
Jose Carlos <sipox11@gmail.com>
2017-09-30 14:13:31 +0200

    Switch project to new directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
   resources :problems
   resources :professionals
-  get 'videocall/vidyoconnector'
 
   root to: "pages#home"
   get '/videocall', to: "videocall#vidyoconnector"
