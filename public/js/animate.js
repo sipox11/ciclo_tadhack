6b85392eebe007756e5f7f237e6b8c8170c82736
Jose Carlos <sipox11@gmail.com>
2017-09-30 14:21:55 +0200

    Add main website to public resources

---
+++ public/js/animate.js
@@ -0,0 +1,14 @@
+function flip(){
+$( ".login" ).toggle();
+$( ".description" ).toggle();
+};
+
+function prof(){
+$( ".profesional" ).toggle();
+};
+
+function subcat(){
+$(".categories").toggle();
+$(".subcategories").toggle();
+
+};
